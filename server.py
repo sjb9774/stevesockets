09b1d5dee6eb9d7a5113f18e6968b6b720c7a0be
Stephen Biston <sjb9774@gmail.com>
2018-01-13 23:54:16 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,248 @@
+import logging
+import socket, select, threading
+
+
+class ConnectionPool:
+
+    def __init__(self):
+        self.cursor = 0
+        self.connections = []
+
+    def __next__(self):
+        return self.next()
+
+    def add(self, connection):
+        self.connections.append(connection)
+
+    def remove(self, connection):
+        self.connections.remove(connection)
+
+    def next(self):
+        return self.connections[self.inc_cursor()]
+
+    def get_cursor(self):
+        return self.cursor
+
+    def inc_cursor(self):
+        self.cursor += 1
+        self.cursor %= len(self.connections)
+        return self.get_cursor()
+
+    def dec_cursor(self):
+        self.cursor -= 1
+        self.cursor %= len(self.connections)
+        return self.get_cursor()
+
+
+class SocketConnection:
+
+    def __init__(self, sck, address, port, logger=None):
+        self.socket = sck
+        self.address = address
+        self.port = port
+        self.handshook = False
+        self.to_be_closed = False
+        self.logger = logger if logger else logging.getLogger()
+
+    def close(self):
+        self.logger.debug("Closing connection at {addr}:{port}".format(addr=self.address, port=self.port))
+        self.socket.close()
+
+    def mark_handshook(self):
+        self.handshook = True
+
+    def is_handshook(self):
+        return self.handshook
+
+    def mark_for_closing(self):
+        self.to_be_closed = True
+
+    def is_to_be_closed(self):
+        return self.to_be_closed
+
+class SocketServer:
+
+    def __init__(self, address=('127.0.0.1', 9000), logger=None):
+        self.address = address
+        self.listening = False
+        self.set_logger(logger if logger else logging.getLogger())
+        self.connections = []
+
+    def _create_socket(self):
+        self.logger.debug("Creating socket")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.socket.bind(self.address)
+        except OSError as err:
+            self.logger.error("Couldn't bind to socket: '{msg}'".format(msg=err.args))
+            self.socket = None
+
+    def dlisten(self):
+        """ Creates, starts, and returns a threading.Thread that runs the `listen` method """
+        t = threading.Thread(target=self.listen)
+        t.start()
+        return t
+
+    def set_logger(self, logger):
+        self.logger = logger
+
+    def listen(self):
+        """ Creates and binds a socket connection at `address` on `port`, then listens for incoming
+            client connections, reads data from them and sends back the return value of `connection_handler`
+            as a response. Business logic should be handled in `handle_message` in subclasses,
+            unless there are specialized cases for connections that need to be handled. """
+        self._create_socket()
+        if not self.socket:
+            self.logger.warn("Socket not successfully initialized, check logs for details. Aborting listen()")
+            self.stop_listening()
+            self._stop_server()
+        else:
+            self.logger.debug("Listening on socket")
+            self.socket.listen(100)
+
+            self.listening = True
+            while self.listening:
+                conn, _, _ = select.select([self.socket], [], [], .1)
+                for c in conn:
+                    sck, addr = self.socket.accept()
+                    self.logger.debug("{addr}".format(addr=addr))
+                    connection = SocketConnection(sck, addr[0], addr[1], logger=self.logger)
+                    self.connections.append(connection)
+                    self.logger.debug("New connection created at {addr}:{port}".format(addr=self.connections[-1].address, port=self.connections[-1].port))
+                    self.logger.debug("Total connections: {n}".format(n=len(self.connections)))
+                # this list will be the list of valid connections left (after creating new connections and closing old ones)
+                # after we've finished looping through all the currently active connections
+                new_connection_list = self.connections[:]
+                for connection in self.connections:
+                    if connection.is_to_be_closed():
+                        self.logger.warn("Connection @ {addr}:{port} to be closed before recieving data? closing".format(addr=connection.address, port=connection.port))
+                        new_connection_list.remove(connection)
+                        connection.close()
+                        continue
+                    try:
+                        avail_data, _, _ = select.select([connection.socket], [], [], .1)
+                        if avail_data:
+                            response = self.connection_handler(connection)
+                            if response:
+                                self.logger.debug("Sending message {r}".format(r=response.encode() if hasattr(response, "encode") else response))
+                                connection.socket.sendall(response.encode() if hasattr(response, "encode") else response)
+                            if connection.is_to_be_closed():
+                                self.logger.debug("Closing connection marked for closure")
+                                new_connection_list.remove(connection)
+                                connection.close()
+                    except socket.error as err:
+                        self.logger.warning("Socket error '{err}'".format(err=err))
+                        new_connection_list.remove(connection)
+                        connection.close()
+                    except KeyboardInterrupt as err:
+                        self.logger.warn("Manually interrupting server")
+                        self.stop_listening()
+                        break
+                self.connections = new_connection_list
+            self.stop_listening()
+            self._stop_server()
+
+    def connection_handler(self, connection):
+        """ Takes a SocketConnection as an argument and passes data recieved from it
+            to `handle_message` which should return the appropriate response for the
+            application. Can be overriden for preprocessing connections/data before
+            building a response such as decoding/encoding data before it's passed to
+            the business logic portion of your application """
+        data = connection.socket.recv(4096)
+        if data:
+            response = self.handle_message(connection, data)
+            return response
+        else:
+            return None
+
+    def handle_message(self, connection, data):
+        """ Should be overriden by subclasses. Responsible for taking raw bytes and
+            returning the business logic response in bytes """
+        return "Hello world"
+
+    def _stop_server(self):
+        self.logger.debug("Closing {n} connections".format(n=len(self.connections)))
+        [c.close() for c in self.connections]
+        self.logger.debug("Closing server socket")
+        self.socket.close()
+        self.logger.debug("Server done listening")
+
+    def stop_listening(self):
+        """ Sets a flag for the server to stop taking new requests and then gracefully close all
+            connections after finishing processing the current pool. """
+        self.logger.debug("Stopping listening")
+        self.listening = False
+
+from stevesockets.websocket import WebSocketFrame
+import base64, hashlib
+
+class WebSocketServer(SocketServer):
+
+    WEBSOCKET_MAGIC = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+    def __init__(self, address=('127.0.0.1', 9000), logger=None):
+        super(WebSocketServer, self).__init__(address=address, logger=logger)
+        self.fragmented_messages = {}
+
+    def connection_handler(self, conn):
+        data = conn.socket.recv(4096)
+        self.logger.debug("Websocket server handling data: {data}".format(data=data))
+        if not conn.is_handshook():
+            return self.handle_websocket_handshake(conn, data)
+        else:
+            frame = WebSocketFrame.from_bytes(data)
+            if not bool(frame.fin):
+                self.fragmented_messages.setdefault(conn, [])
+                self.fragmented_messages[conn].append(frame)
+                return None
+            else: # is final frame
+                message_fragments = self.fragmented_messages.get(conn)
+                if message_fragments:
+                    complete_message = "".join(f.message for f in message_fragments)
+                    complete_message += frame.message
+                    self.fragmented_messages[conn] = []
+                # check for special frames (PING, close, etc)
+                elif frame.opcode == WebSocketFrame.OPCODE_PING:
+                    self.logger.debug("Recieved PING, sending PONG")
+                    return WebSocketFrame(message=frame.message, opcode=WebSocketFrame.OPCODE_PONG).to_bytes()
+                elif frame.opcode == WebSocketFrame.OPCODE_PONG:
+                    self.logger.debug("Recieved PONG, ignoring")
+                    return None # ignore pongs
+                elif frame.opcode == WebSocketFrame.OPCODE_CLOSE:
+                    self.logger.debug("Recieved close frame with message: {m}".format(m=frame.message))
+                    conn.to_be_closed = True
+                    return WebSocketFrame(message=frame.message, opcode=WebSocketFrame.OPCODE_CLOSE).to_bytes()
+                else: #normal message
+                    complete_message = frame.message
+            response = self.handle_message(conn, complete_message)
+            return WebSocketFrame(message=response).to_bytes()
+
+    def _get_websocket_accept(self, key):
+        return base64.b64encode(hashlib.sha1((key + self.WEBSOCKET_MAGIC).encode()).digest()).decode()
+
+    def handle_websocket_handshake(self, conn, data):
+        self.logger.debug("Starting handshake")
+        split_data = data.decode().split("\r\n")
+        method, path, http = split_data[0].split(" ")
+        headers = {}
+        try:
+            for header in split_data[1:]:
+                if ": " in header:
+                    key, value = header.split(": ")
+                    headers[key] = value
+        except TypeError as err:
+            self.logger.error("Malformed headers in client handshake, closing connection")
+            conn.mark_for_closing()
+            response = ""
+        except BaseException as err:
+            self.logger.error("Unexpected error encountered, closing connection: {msg}".format(msg=err))
+            conn.mark_for_closing()
+            response = ""
+        else:
+            accept = self._get_websocket_accept(headers.get("Sec-WebSocket-Key"))
+            response = "".join(["HTTP/1.1 101 Switching Protocols\r\n",
+                                "Upgrade: websocket\r\n",
+                                "Connection: Upgrade\r\n",
+                                "Sec-WebSocket-Accept: {accept}\r\n\r\n".format(accept=accept)])
+            conn.mark_handshook()
+        return response
---
+++ websocket.py
@@ -0,0 +1,119 @@
+from logging import getLogger as get_logger
+
+logger = get_logger(__name__)
+
+def bits(byte, p=7):
+    if byte > 255 or byte < 0:
+        raise ValueError("Bytes can't be greater than 255 or less than 0")
+    result = '1' if byte >= (2**p) else '0'
+    if result == '0' and p > 0:
+        result += bits(byte, p-1)
+    elif p > 0:
+        leftover = byte - 2**p
+        result += bits(leftover, p-1)
+    return result
+
+def bits_value(bits):
+    if type(bits) != str:
+        raise ValueError("Input should be a bit string")
+    p = len(bits) - 1
+    total = 0
+    for i, bit in enumerate(bits):
+        if bit not in ("1", "0"):
+            raise ValueError("Invalid bit string")
+        total += int(bit) * (2**(p-i))
+    return total
+
+def to_binary(n, pad_to=None):
+    r = n
+    s = ""
+    while r > 0:
+        s += str(r % 2)
+        r = r // 2
+    result = s[-1::-1]
+    if pad_to and len(result) < pad_to:
+        while len(result) < pad_to:
+            result = "0" + result
+    return result
+
+import select
+import random
+
+
+class WebSocketFrame:
+
+    OPCODE_CLOSE    = 8
+    OPCODE_PING     = 9
+    OPCODE_PONG     = 10
+
+    def __init__(self, fin=1, opcode=1, rsv=0, mask=None, message=None):
+        self.fin = fin
+        self.opcode = opcode
+        self.mask_flag = 1 if mask else 0
+        self.mask = mask
+        self.message = message
+        self.payload_length = len(message)
+        self.rsv = rsv
+
+    def to_bytes(self):
+        full_bit_str = ""
+        full_bit_str += to_binary(self.fin, pad_to=1)
+        full_bit_str += to_binary(self.rsv, pad_to=3)
+        full_bit_str += to_binary(self.opcode, pad_to=4)
+        full_bit_str += to_binary(self.mask_flag, pad_to=1)
+        if self.payload_length <= 125:
+            full_bit_str += to_binary(self.payload_length, pad_to=7)
+        elif self.payload_length == 126:
+            full_bit_str += to_binary(126, pad_to=7)
+            full_bit_str += to_binary(self.payload_length, pad_to=16)
+        elif self.payload_length > 126:
+            full_bit_str += to_binary(127, pad_to=7)
+            full_bit_str += to_binary(self.payload_length, pad_to=64)
+        if bool(self.mask_flag):
+            mask_bits = to_binary(self.mask, pad_to=32)
+            full_bit_str += mask_bits
+            mask_values = [bits_value(mask_bits[x:x+8]) for x in range(0, len(mask_bits), 8)]
+            masked_message = []
+            for i, char in enumerate(self.message):
+                masked_message.append(ord(char) ^ mask_values[i % 4])
+            full_bit_str += "".join(to_binary(x, pad_to=8) for x in masked_message)
+        else:
+            msg = [to_binary(x, pad_to=8) for x in (ord(y) for y in self.message)]
+            full_bit_str += "".join(msg)
+        split_to_bytes = [bits_value(full_bit_str[x:x+8]) for x in range(0, len(full_bit_str), 8)]
+        return bytes(split_to_bytes)
+
+    @staticmethod
+    def generate_mask():
+        return random.randint(0, (2 ** 32) - 1)
+
+    @classmethod
+    def from_bytes(cls, in_bytes):
+        #import pudb; pudb.set_trace()
+        full_bit_str = "".join([bits(byte) for byte in in_bytes])
+        bit_generator = (bit for bit in full_bit_str)
+        get_next_bits = lambda x: "".join(next(bit_generator) for i in range(x))
+
+        fin = bits_value(get_next_bits(1))
+        rsv = bits_value(get_next_bits(3)) # can be safely ignored
+        opcode = bits_value(get_next_bits(4))
+        mask_flag = bits_value(get_next_bits(1))
+        bits_9_15_val = bits_value(get_next_bits(7))
+        if bits_9_15_val <= 125:
+            payload_length = bits_9_15_val
+        elif bits_9_15_val == 126:
+            payload_length = bits_value(get_next_bits(16))
+        elif bits_9_15_val == 127:
+            payload_length = bits_value(get_next_bits(64))
+
+        mask = get_next_bits(32) if bool(mask_flag) else None
+        message = ""
+        for x in range(payload_length):
+            char_data = get_next_bits(8)
+            if bool(mask_flag):
+                char = chr(bits_value(char_data) ^ bits_value(mask[(x % 4) * 8:((x % 4) * 8) + 8]))
+            else:
+                char = chr(bits_value(char_data))
+            message += char
+        f = cls(fin=fin, opcode=opcode, rsv=rsv, mask=bits_value(mask) if mask else None, message=message)
+        return f
