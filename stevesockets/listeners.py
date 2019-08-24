cedec3ab050bc4f33c95665f98b7b625ef2d4908
Stephen <sjb9774@gmail.com>
2019-08-23 23:33:22 -0500

    Refactor Listeners for more centralized utility

--- stevesockets/listeners.py
+++ stevesockets/listeners.py
@@ -5,22 +5,38 @@ from stevesockets import LOGGER_NAME
 import json
 
 
-class CloseListener(Listener):
+class WebFrameListener(Listener):
 
-    def observe(self, message, *args, connection=None, **kwargs):
+    def __init__(self, fn=None):
+        super(WebFrameListener, self).__init__(fn)
+        self.logger = logging.getLogger(LOGGER_NAME)
+
+    def close_connection(self, connection, message=None):
         # as soon as we know a CLOSE frame is received, send no other messages and close connection
         connection.clear_messages()
-        connection.queue_message(WebSocketFrame.get_close_frame(message.message).to_bytes())
-        connection.flush_messages()
+        msg = WebSocketFrame.get_close_frame(message)
+        self.send_frame(connection, msg, flush_immediately=True)
         connection.close()
 
+    def send_frame(self, connection, frame, flush_immediately=False):
+        frame_bytes = frame.to_bytes()
+        self.logger.debug(f'Sending bytes: {frame_bytes}')
+        connection.queue_message(frame_bytes)
+        if flush_immediately:
+            connection.flush_messages()
 
-class PingListener(Listener):
+
+class CloseListener(WebFrameListener):
 
     def observe(self, message, *args, connection=None, **kwargs):
-        logger = logging.getLogger(LOGGER_NAME)
-        logger.debug("Received PING, sending PONG")
-        connection.queue_message(WebSocketFrame.get_pong_frame(message=message).to_bytes())
+        self.close_connection(connection, message=message.message)
+
+
+class PingListener(WebFrameListener):
+
+    def observe(self, message, *args, connection=None, **kwargs):
+        msg = WebSocketFrame.get_pong_frame(message=message)
+        self.send_frame(connection, msg)
 
 
 class TextListener(Listener):
@@ -30,14 +46,14 @@ class TextListener(Listener):
         logger.debug(f"Handling text message: '{message.message}'")
 
 
-class MessageSynchronizer(Listener):
+class MessageSynchronizer(WebFrameListener):
 
     def observe(self, message, *args, connection=None, server=None, **kwargs):
         headers = WebSocketFrameHeaders(payload_length=len(message.message))
         server_message = WebSocketFrame(headers=headers, message=message.message)
         for server_connection in server.connections:
             if server_connection != connection:
-                server_connection.queue_message(server_message.to_bytes())
+                self.send_frame(server_connection, server_message)
 
 
 class JSONListener(Listener):
